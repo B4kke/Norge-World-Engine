b355eba3c04e9a958b2da24490499b63c8b91279
B4kke <msb.marius@gmail.com>
2026-08-18 16:45:20 +0200

    Type Preview 1 renderer boundary

---
+++ apps/world-viewer/src/preview1.d.ts
@@ -0,0 +1,10 @@
+export interface Preview1RendererStats {
+  terrain_vertices: number;
+  terrain_triangles: number;
+  road_paths: number;
+  building_footprints: number;
+  source_backed_building_heights: number;
+  unresolved_building_heights: number;
+  debug_road_width_m: number;
+  debug_unresolved_building_height_m: number;
+}
